ecdb3f41525f96c5ca1b76416ca879d1ffb34bfd
VladTrofimchenko <trofimchenko.vladtrof@yandex.ru>
2023-04-22 16:26:14 +0300

    clean

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/repository/jpa/JpaTgChatRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.tinkoff.edu.java.scrapper.repository.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.tinkoff.edu.java.scrapper.entity.jpa.TgChatEntity;
-
-public interface JpaTgChatRepository extends JpaRepository<TgChatEntity, Long> {
-}
